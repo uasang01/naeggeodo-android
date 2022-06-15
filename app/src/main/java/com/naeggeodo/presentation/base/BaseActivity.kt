6b7dbee88c4b28895ddab1ac3b79929d992e917c
김상훈 <uasang01@name.com>
2022-06-16 04:26:16 +0900

    feat : set up projcet, added login layout

---
+++ app/src/main/java/com/naeggeodo/presentation/base/BaseActivity.kt
@@ -0,0 +1,44 @@
+package com.naeggeodo.presentation.base
+
+import android.os.Bundle
+import androidx.annotation.LayoutRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+
+abstract class BaseActivity<T: ViewDataBinding>
+    (@LayoutRes private val layoutId: Int): AppCompatActivity() {
+
+    protected lateinit var binding: T
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        beforeSetContentView()
+
+        super.onCreate(savedInstanceState)
+
+        // ignore dark mode
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+
+        binding = DataBindingUtil.setContentView(this, layoutId)
+
+        initView()
+        initViewModel()
+        initListener()
+        afterOnCreate()
+    }
+
+    protected open fun beforeSetContentView() {}
+    protected open fun initView() {}
+    protected open fun initViewModel() {}
+    protected open fun initListener() {}
+    protected open fun afterOnCreate() {}
+
+
+//    protected fun shortShowToast(msg: String) =
+//        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+//
+//    protected fun longShowToast(msg: String) =
+//        Toast.makeText(this, msg, Toast.LENGTH_LONG).show()
+
+}
---
+++ app/src/main/java/com/naeggeodo/presentation/view/LoginActivity.kt
@@ -0,0 +1,9 @@
+package com.naeggeodo.presentation.view
+
+import com.naeggeodo.presentation.R
+import com.naeggeodo.presentation.base.BaseActivity
+import com.naeggeodo.presentation.databinding.ActivityLoginBinding
+
+class LoginActivity : BaseActivity<ActivityLoginBinding>(R.layout.activity_login) {
+
+}
